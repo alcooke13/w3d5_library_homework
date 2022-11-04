f262700a90808ff9fa3ee65d683ca25a01af8e42
Alistair Cooke <alcooke@alistairs-air.lan>
2022-11-04 12:17:23 +0000

    homework setup complete

---
+++ controllers/controller.py
@@ -0,0 +1,4 @@
+from app import app
+from models.book import *
+from models.books import *
+from flask import render_template
---
+++ models/books.py
@@ -0,0 +1,7 @@
+from models.book import Book
+
+book1 = Book("The Best Book Ever", "Jeff Jefferson", "Horror")
+book2 = Book("The Mysterious Mystery", "Sam Sammerson", "Thriller")
+book3 = Book("The Life Of A Goose", "Goose Duck", "Autobiography")
+
+list_of_books = [book1, book2, book3]
