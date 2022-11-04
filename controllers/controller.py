584dc7ab3e821c89be653041eaaca0fe8f12ea8c
Alistair Cooke <alcooke@alistairs-air.lan>
2022-11-04 14:12:25 +0000

    added ability to choose book from list and goes to new page

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,3 +2,12 @@ from app import app
 from models.book import *
 from models.books import *
 from flask import render_template
+
+@app.route('/books')
+def index():
+    return render_template('index.html', title="List Of Books", books=list_of_books)
+
+@app.route('/books/<index>')
+def choose_book(index):
+    one_book = list_of_books[int(index)]
+    return render_template('single_book.html', title="Chosen Book", book=one_book)
